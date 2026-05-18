eab90a0b068530bffedf31de87b5bc1586189f2c
RamonSest0 <ramon.sesto@gmail.com>
2026-05-18 11:10:53 -0300

    aula 37

---
+++ aula37.py
@@ -0,0 +1,17 @@
+"""
+Lista em Python
+Tipo list - Mutável
+Suporta vários valores em qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis: append, insert, pop, del, clear, extend, +
+"""
+
+string = 'ABCDE' 
+#         0     1      2      3 - indicice positivo
+lista = [123, True, 'Ramon', 1.7] 
+#        -4     -3    -2     -1 - indice negativo
+lista[2] = 'AJ'
+lista[0] = 321
+print(lista)
+# print(bool(lista)) falsy
+# print(lista, type(lista))
