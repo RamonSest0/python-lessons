02bee55f2f18d3f91caf125d36779d49efd583a7
RamonSest0 <ramon.sesto@gmail.com>
2026-05-19 08:41:06 -0300

    aula 38

---
+++ aula38.py
@@ -0,0 +1,16 @@
+"""
+
+Métodos úteis:
+
+append, insert, pop, del, clear, extend, 
+Create Read Update Delete
+Criar, ler, alterar, apagar = lista[i] (CRUD)
+
+"""
+
+lista = [1, 2, 3, 4]
+lista[2] = 30 # Update
+del lista[0] # Delete
+lista.append(5) # Create
+lista.pop() # remove o ultimo item
+print(lista)
