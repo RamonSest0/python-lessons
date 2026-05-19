00d05e2a8b0bced52eae555767bb8866a8e3ae5c
RamonSest0 <ramon.sesto@gmail.com>
2026-05-19 10:33:02 -0300

    exercicio 8

---
+++ exercicios/exercicio8.py
@@ -0,0 +1,12 @@
+"""
+Exercício
+Exiba os índices da lista
+
+"""
+
+lista = ['Ramon', 'Ana Julia', 'AR']
+
+indices = range(len(lista))
+
+for i in indices:
+    print(f'{lista[i]} está no índice {i}')
