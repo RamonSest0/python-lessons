a004ffa33644bb894c9a2574e86a1c884ae6ca20
RamonSest0 <ramon.sesto@gmail.com>
2026-05-19 09:43:47 -0300

    att aula 40

--- aula40.py
+++ aula40.py
@@ -3,9 +3,15 @@
 extend - estende a lista
 """
 
+# + 
 
 lista_a = [1, 2, 3]
 lista_b = [4, 5, 6]
 lista_c = lista_a + lista_b
-lista_d = lista_a.extend(lista_b)
-print(lista_d)
+print(lista_c)
+
+# extend()
+
+lista_d = lista_a.extend(lista_b) # extend() realiza uma ação, mas não retorna nada. 
+print(lista_d) # Por isso lista_d = None
+print(lista_a) # podemos ver que extend() realizou a ação devida na lista_a
