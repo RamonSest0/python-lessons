487d14d1ad4d35ebcf1108da04eb7c001cefda17
RamonSest0 <ramon.sesto@gmail.com>
2026-05-20 13:22:43 -0300

    att aula 43

--- aulas/aula43.py
+++ aulas/aula43.py
@@ -1,5 +1,12 @@
 """
-Introdução ao desempacotamento + tuples (tuplas)
+Introdução ao desempacotamento + tuples
+_ <- variavel que não vai ser usada (convenção Python)
+* <- dito de modo simples usamos para que o "resto" da lista fique na variável (*variavel)
 """
 
-nomes = ['Ramon', 'Ana Julia', 'Luiz']
+nome1, nome2, nome3, nome4 = ['Ramon', 'Ana Julia', 'Luiz', ':)'] # o número de varias e valores precisam ser o mesmo, se não gera um erro
+print(nome2)
+
+_, ingrediente, *outros_ingredientes = ['arroz', 'batata', 'feijão', 'carne', 'tomate']
+print(ingrediente)
+print(outros_ingredientes) # retorna o resto da list
