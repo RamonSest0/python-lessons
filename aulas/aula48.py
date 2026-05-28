6de19e7766523494d4e4e9bfca4e4019a11c0a9a
RamonSest0 <ramon.sesto@gmail.com>
2026-05-28 04:52:19 -0300

    aula 48

---
+++ aulas/aula48.py
@@ -0,0 +1,14 @@
+"""
+Pratica de acesso de listas dentro de listas
+
+"""
+
+salas = [
+    ['Ramon', 'Ana Julia'],
+    ['Elaine'],
+    ['Jorge', 'Marcos', 'Marta', (0, 10, 20, 30, 40)]
+]
+
+print(salas[1][0])
+print(salas[2][1])
+print(salas[2][3][3])
