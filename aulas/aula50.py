aa2854ff60ce2c50099c9dcb283df926342f310c
RamonSest0 <ramon.sesto@gmail.com>
2026-05-28 05:33:54 -0300

    aulas 49 e 50

---
+++ aulas/aula50.py
@@ -0,0 +1,10 @@
+# Desempacotamento em chamadas
+# de métodos de funções
+
+string = 'ABC'
+lista = ['Maria', 'Helena', 'Eduarda']
+tupla = 'Python', 'é', 'legal'
+
+a, b, c = lista
+print(*lista)
+print(*string)
