5a2de061fb6260252b55456109b94fda9c09b6ab
RamonSest0 <ramon.sesto@gmail.com>
2026-06-13 06:50:39 -0300

    att aula57

--- aulas/aula57.py
+++ aulas/aula57.py
@@ -3,9 +3,13 @@ Retorno de valores das funções (return)
 """
 
 def soma(x, y):
+    if x > 10:
+        return [10, 20]
     return x + y # para todo a execução e retorna o que foi pedido | ou seja, não podemos ter nada abaixo do return
 
 
 soma1 = soma(2, 2)
 soma2 = soma(3, 3)
-print(soma1 + soma2)
+print(soma1)
+print(soma2)
+print(soma(11, 60))
