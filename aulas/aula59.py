92f32caa82a4d530ae48e0f6bc2eaac1c8a0848c
RamonSest0 <ramon.sesto@gmail.com>
2026-07-09 17:35:32 -0300

    aula59

--- aulas/aula59.py
+++ aulas/aula59.py
@@ -3,3 +3,11 @@ Higher Order Functions
 Funções de Primeira Classe
 """
 
+def saudacao(msg, nome):
+    return f'{msg}, {nome}'
+
+def executa(funcao, *args):
+    return funcao(*args)
+
+print(executa(saudacao, 'Bom dia', 'Ramon'))
+print(executa(saudacao, 'Bom dia', 'Ana Julia'))
