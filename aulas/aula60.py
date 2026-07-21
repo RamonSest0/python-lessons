ef7b2cf562fae492c22906affa1ec030f57d0c6e
RamonSest0 <ramon.sesto@gmail.com>
2026-07-21 10:37:55 -0300

    aula60

---
+++ aulas/aula60.py
@@ -0,0 +1,16 @@
+"""
+Closure e funções que retornam outras funções
+"""
+
+def criar_saudacao(saudacao):
+    def saudar(nome):
+        return f'{saudacao}, {nome}'
+    return saudar
+    
+
+
+falar_bom_dia = criar_saudacao('Bom dia')
+print(falar_bom_dia('Ramon'))
+
+for nome in ['Ramon', 'Ana Julia']:
+    print(falar_bom_dia(nome))
